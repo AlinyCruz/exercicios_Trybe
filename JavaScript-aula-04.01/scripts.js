2721ca5da30ee581db437d30332a7f7b6f13cd9c
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-07-20 16:23:17 -0300

    Iniciando o JavaScript

---
+++ JavaScript-aula-04.01/scripts.js
@@ -0,0 +1,13 @@
+const myName = "Aline Cruz"
+const birthCity = "São Paulo"
+let birthYear = 1989
+
+console.log(myName)
+console.log(birthCity)
+console.log(birthYear)
+
+birthYear = 2030
+console.log(birthYear)
+
+birthCity = "Bahia";
+console.log(birthCity)
