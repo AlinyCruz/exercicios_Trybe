64190d78012b8bcdbd92b3681fad9e1ef721416e
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-07-20 18:31:01 -0300

    Fiz somas e testes

--- JavaScript-aula-04.01/scripts.js
+++ JavaScript-aula-04.01/scripts.js
@@ -1,13 +1,42 @@
-const myName = "Aline Cruz"
-const birthCity = "São Paulo"
-let birthYear = 1989
+// const myName = "Aline Cruz"
+// const birthCity = "São Paulo"
+// let birthYear = 1989
 
-console.log(myName)
-console.log(birthCity)
-console.log(birthYear)
+// console.log(myName)
+// console.log(birthCity)
+// console.log(birthYear)
 
-birthYear = 2030
-console.log(birthYear)
+// birthYear = 2030
+// console.log(birthYear)
 
-birthCity = "Bahia";
-console.log(birthCity)
+// birthCity = "Bahia";
+// console.log(birthCity)
+
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+
+// const base = 5;
+// let height = 8;
+
+// let area = base * height;
+
+// console.log(area);
+
+let lado1 = 4;
+let lado2 = 2;
+let lado3 = lado1;
+let lado4 = lado2;
+
+const perimeter = lado1 + lado2 + lado3 + lado4;
+
+console.log(perimeter);
