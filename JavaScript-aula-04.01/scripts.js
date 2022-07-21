db054ed34dc1f28c5d4caee68c205d4e4e26e7ff
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-07-21 18:26:30 -0300

    Exercicio if else

--- JavaScript-aula-04.01/scripts.js
+++ JavaScript-aula-04.01/scripts.js
@@ -32,11 +32,23 @@
 
 // console.log(area);
 
-let lado1 = 4;
-let lado2 = 2;
-let lado3 = lado1;
-let lado4 = lado2;
+// let lado1 = 4;
+// let lado2 = 2;
+// let lado3 = lado1;
+// let lado4 = lado2;
 
-const perimeter = lado1 + lado2 + lado3 + lado4;
+// const perimeter = lado1 + lado2 + lado3 + lado4;
 
-console.log(perimeter);
+// console.log(perimeter);
+
+const nota = 8;
+
+if (nota >= 80) {
+    console.log ("Parabéns, você foi aprovada(o)!");
+} 
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+if (nota < 60) {
+   console.log("Você foi reprovada(o)");
+}
